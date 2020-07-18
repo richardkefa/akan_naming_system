5e1672331edc1cf07c6b904de52dc7e024263817
richardkefa <richardkefa@gmail.com>
2020-07-18 13:26:01 +0300

    add arrays

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var maleName=["Kwasi", "Kwadwo", "Kwabena", "Kwatu", "Yaw", "Koh", "Kwame"];
+var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"];
+var daysOfWeek=["Sunday", "Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday"];
