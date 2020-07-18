a2f963a745d32a95705db4e0fbc4720098d769f5
richardkefa <richardkefa@gmail.com>
2020-07-19 00:50:23 +0300

    add form and button

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwatu", "Yaw", "Koh", "Kwame"];
 var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa","Afua", "Ama"];
 var daysOfWeek=["Sunday", "Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday"];
 
-function gettingAkanName (){
-  var day=parseInt(document.getElementById(day));
-  var month=parseInt(document.getElementById(month));
-  var year=parseInt(document.getElementById(year));
-  var gender=document.getElementByClass(gender);
+function gettingAkanName(){
+  var day=parseInt(document.getElementById("day").value);
+  var month=parseInt(document.getElementById("month").value);
+  var year=parseInt(document.getElementById("year").value);
+  var gender=getSex();
+  
   var validDate=(day>0 && day<=31 && month>0 && month<=12 && year>0);
   var validGender=(gender=="male" || gender=="female");
   if (day<0 || day>31){
@@ -33,6 +34,14 @@ function gettingAkanName (){
   }
   else if (gender=="female"){
     alert("You were born on "+daysOfWeek[birthDay]+" Your Akan name is "+femaleNames[birthDay]);
+  }
+  function getSex(){
+    var gender = document.getElementsByName("gender").value;
+    for(x=0; x < gender.length(); x++){
+        if (gender[x].checked) {
+          return(gender[x].value);
+        }
+    }
   }
 }
 
